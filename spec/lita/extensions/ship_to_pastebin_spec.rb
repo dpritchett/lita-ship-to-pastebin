2048df2e14b22e345947fc92e98b54eaf81e270c
Daniel J. Pritchett <dpritchett@gmail.com>
2018-04-24 06:23:56 -0500

    Cleaned out specs with bigtext tested separately

--- spec/lita/extensions/ship_to_pastebin_spec.rb
+++ spec/lita/extensions/ship_to_pastebin_spec.rb
@@ -1,24 +1,10 @@
-require 'pry'
-require "spec_helper"
-
-# TODO: require this instead somehow
-load "lib/lita-bigtext.rb"
+require 'spec_helper'
 
 describe Lita::Extensions::ShipToPastebin, lita: true do
-  # do the robot
-  let(:robot) { Lita::Robot.new(registry) }
-  let(:bigtexter) { Lita::Handlers::Bigtext.new(robot) }
-
   subject { Lita::Extensions::ShipToPastebin.new }
-
-  it 'does a bigtext' do
-    result = bigtexter.longtext
-
-    expect(result).to match(/http/i)
-  end
 
   it 'saves text to pastebin' do
     actual = subject.save_to_pastebin 'hey john', title: 'hey there john'
-    expect(actual).to match /https:\/\/pastebin/
+    expect(actual).to match(%r{https:\/\/pastebin/})
   end
 end
